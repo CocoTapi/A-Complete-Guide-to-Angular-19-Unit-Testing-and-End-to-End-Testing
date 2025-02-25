f1f025c1f30d36966daeea7f3dd5d01b82b36882
CocoTapi <shioriavino@gmail.com>
2025-02-24 20:57:31 -0500

    add test for add and subtract functions

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,54 @@
+import { CalculatorService } from "./calculator.service";
+import { CoursesService } from "./courses.service";
+import { LoggerService } from "./logger.service";
+import { TestBed } from '@angular/core/testing'
+  
+describe('CalculatorService', () => {
+    let calculator: CalculatorService;
+    let loggerSpy: any; 
+
+    // this is a setup to avoid repeating codes
+    beforeEach(() => {
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+        calculator = new CalculatorService(loggerSpy);
+9898
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {provide: LoggerService, useValue: loggerSpy}
+            ]
+        })
+
+        calculator = TestBed.inject<CoursesService>(CoursesService);
+
+        
+    })
+
+    it ('should add two numbers', () => {
+
+        // const logger = jasmine.createSpyObj('LoggerService', ["log"]);
+        // const logger = new LoggerService();
+        // spyOn(logger, 'log');
+
+        // prepare a component or service
+        // const calculator = new CalculatorService(logger);
+
+        // trigger the operation
+        const result = calculator.add(2, 2);
+
+        // expecting result
+        expect(result).toBe(4);
+
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    }); 
+
+    it ('should subtract two numbers', () => {
+        // const logger = jasmine.createSpyObj('LoggerService', ["log"]); 
+        // const calculator = new CalculatorService(logger);
+
+        const result = calculator.subtract(2, 2);
+
+        expect(result).toBe(0, "subtraction result");    
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+})
